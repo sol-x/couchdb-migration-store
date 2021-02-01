cd5c3b0ed0fb816a3bb0312f98cfb969d4ba46c5
Leon Song <sxwei123@users.noreply.github.com>
2021-02-01 18:33:18 +0800

    Improve save method by replacing same document instead of recreate the database

---
+++ README.md
@@ -0,0 +1,3 @@
+# CouchDB Node Migrate Store
+
+This repo implements a custom [node-migrate](https://github.com/tj/node-migrate#custom-state-storage) store with CouchDB. For more information, please check `node-migrate` documentation.
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ interface MigrationData {
 const MIGRATION_DB_NAME = "migrations";
 
 type MigrationSaveCallback = (err?: Error) => void;
-type MigrationLoadCallback = (err: Error | undefined, data?: MigrationData | {}) => void;
+type MigrationLoadCallback = (
+  err: Error | undefined,
+  data?: MigrationData | {}
+) => void;
 
 class CouchDbMigrationStore {
   couchDbUrl: string;
@@ -19,45 +22,44 @@ class CouchDbMigrationStore {
     const couchDbUrl = process.env.COUCHDB_URL;
     if (!couchDbUrl) {
       throw new Error(
-        "Environment variable COUCHDB_URL must be set to use couchdb-migration-store",
+        "Environment variable COUCHDB_URL must be set to use couchdb-migration-store"
       );
     }
-    this.couchDbWaitTime = parseInt(process.env.COUCHDB_WAIT_TIME || "0") || 10 * 1000;
+    this.couchDbWaitTime =
+      parseInt(process.env.COUCHDB_WAIT_TIME || "0") || 10 * 1000;
     this.couchDbUrl = couchDbUrl;
   }
 
-  private async waitForCouchdb(): Promise<void> {
+  private async waitForCouchdb(): Promise<Nano.DocumentScope<MigrationData>> {
     await waitOn({
       resources: [this.couchDbUrl],
       timeout: this.couchDbWaitTime,
     });
+
+    const nano = Nano(this.couchDbUrl);
+    const allDbs = await nano.db.list();
+
+    // Create MIGRATION_DB_NAME if it does not exist
+    if (!allDbs.includes(MIGRATION_DB_NAME)) {
+      await nano.db.create(MIGRATION_DB_NAME);
+    }
+    return nano.use<MigrationData>(MIGRATION_DB_NAME);
   }
 
-  async save(migrationData: MigrationData, callback: MigrationSaveCallback): Promise<void> {
+  async save(
+    migrationData: MigrationData,
+    callback: MigrationSaveCallback
+  ): Promise<void> {
     const { lastRun } = migrationData;
-    const migrations = migrationData.migrations.map(migration => ({
+    const migrations = migrationData.migrations.map((migration) => ({
       title: migration.title,
       timestamp: migration.timestamp,
     }));
     const dataToStore = { _id: "1", lastRun, migrations };
 
     try {
-      await this.waitForCouchdb();
-    } catch (err) {
-      callback(err);
-      return;
-    }
-
-    const nano = Nano(this.couchDbUrl);
-    try {
-      await nano.db.destroy(MIGRATION_DB_NAME);
-    } catch {
-    } finally {
-      await nano.db.create(MIGRATION_DB_NAME);
-    }
-
-    try {
-      const db = nano.use<MigrationData>(MIGRATION_DB_NAME);
+      const db = await this.waitForCouchdb();
+      // The insert method will update the document with same _id
       await db.insert(dataToStore);
       callback(undefined);
     } catch (e) {
@@ -67,15 +69,7 @@ class CouchDbMigrationStore {
 
   async load(callback: MigrationLoadCallback): Promise<void> {
     try {
-      await this.waitForCouchdb();
-    } catch (err) {
-      callback(err);
-      return;
-    }
-
-    const nano = Nano(this.couchDbUrl);
-    try {
-      const db = nano.use<MigrationData>(MIGRATION_DB_NAME);
+      const db = await this.waitForCouchdb();
       const data = await db.find({ limit: 1, selector: {} });
       const { docs } = data;
       if (docs.length) {
@@ -84,7 +78,7 @@ class CouchDbMigrationStore {
         callback(undefined, {});
       }
     } catch (e) {
-      callback(undefined, {});
+      callback(e, {});
     }
   }
 }
